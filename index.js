54eaf42438501a4c96b92713e38e3e1ca4a2d0f9
Peetah <peterleemartin@gmail.com>
2024-01-20 12:03:44 -0600

    installed inquirer and set up questions array. started writing out functionality to ask questions in node

--- index.js
+++ index.js
@@ -1,13 +1,54 @@
-// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        message: "Please enter a title for your README.",
+        name: "title"
+    },
+    {
+        message: "Please enter a description for your README.",
+        name: "desc"
+    },
+    {
+        message: "Please enter any installation instructions.",
+        name: "install",
+    },
+    {
+        message: "Please enter any usage information.",
+        name: "usage"
+    },
+    {
+        message: "Please enter any contribution guidelines.",
+        name: "contrib",
+    },
+    {
+        message: "Please enter any test instructions.",
+        name: "test",
+    }
+];
+
+const askUser = (questionArray) => {
+    return inquirer.prompt(questionArray);
+}
+//     .then((answers) => {
+//         console.log(answers);
+//     })
+//     .catch((error) => {
+//         console.error(error);
+//     })
+// };
+askUser(questions);
+console.log(process.argv[2]);
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(`${answers.name}.txt`, JSON.stringify(answers, null, 2), (err) =>
+        err ? console.error(err) : console.log('Success!'))
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() { }
 
 // Function call to initialize app
-init();
+// init();
